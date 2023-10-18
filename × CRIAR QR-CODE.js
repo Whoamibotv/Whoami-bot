5220a2429920eaa7051306e619cea3f3ce345446
Whoamibotv <148023372+Whoamibotv@users.noreply.github.com>
2023-10-18 07:43:17 -0300

    Delete × CRIAR QR-CODE.js

--- × CRIAR QR-CODE.js
+++
@@ -1,6 +0,0 @@
-case 'qrcode': //VNCS XANAA ROBOT
-const tex = encodeURIComponent(body.slice(8))
-if (!tex) return reply('Digite um texto/url que deseja criar um código qr')
-const bufferr = await getBuffer(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${tex}`)
-conn.sendMessage(from, {image: bufferr}, {quoted: info})
-break
